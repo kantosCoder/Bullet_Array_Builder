f03801f3657751e2009bac0f85f427a556ef49a3
kantosCoder <56650488+kantosCoder@users.noreply.github.com>
2020-02-01 03:20:03 +0100

    Done
    done

---
+++ Assets/NOTES_BEHABVIOUR.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NOTES_BEHABVIOUR : MonoBehaviour
+{
+    //This document is made for taking some notes about the game's behabviour, also info about it.
+    /*Game's Autor: kantosCoder
+    
+    
+    Notes:     
+         About the UI layer:  for working is good to have it embeeded with the main camera (to keep it tight),
+         but it can do weird things with the layers, so in order to have it working properly is better to have
+         it on "overlay" mode.
+    
+         
+         
+    */
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
