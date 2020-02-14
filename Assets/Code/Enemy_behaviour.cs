206374c419d879618a09c0d89f23ef88b253fd77
kantosCoder <56650488+kantosCoder@users.noreply.github.com>
2020-02-14 20:35:10 +0100

    Done

--- Assets/Code/Enemy_behaviour.cs
+++ Assets/Code/Enemy_behaviour.cs
@@ -8,6 +8,8 @@ public class Enemy_behaviour : MonoBehaviour
     private float scaleX;
     private float scaleY;
     private float scaleZ;
+    private bool leftlimit = false;
+    private bool rightlimit = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,28 @@ public class Enemy_behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(patrolspeed, 0, 0) * Time.deltaTime;
-        transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+        if (leftlimit) {
+            transform.position += new Vector3(patrolspeed, 0, 0) * Time.deltaTime;
+            transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+        }
+        if (rightlimit){
+            transform.position += new Vector3(-patrolspeed, 0, 0) * Time.deltaTime;
+            transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+        }
+
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (!collision.collider.tag.Equals("Terrain"))
+        {
+            if (leftlimit) { rightlimit = true; }
+            if (rightlimit) { leftlimit = true; }
+        }
+        if (collision.collider.tag.Equals("Wall"))
+        {
+            if (leftlimit) { rightlimit = true; }
+            if (rightlimit) { leftlimit = true; }
+        }
     }
 }
